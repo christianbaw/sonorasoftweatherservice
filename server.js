f09cb1f01f5921e8e54d96af37783e20e2da9427
Christian Bawman <Christiangbm@gmail.com>
2018-11-13 21:40:13 -0700

    fixed heroku

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist'));
+app.use('/js', express.static(__dirname + '/js'));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
