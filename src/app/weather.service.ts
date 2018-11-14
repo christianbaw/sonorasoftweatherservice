28d0bb33f513fb9eb1dfbe1aa0986b7590d5404a
Christian Bawman <Christiangbm@gmail.com>
2018-11-13 21:56:08 -0700

    MOVED API TO HTTPS

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -11,7 +11,7 @@ export class WeatherService {
   constructor(private _http: HttpClient) { }
 
   getdailyForecast(city,scale){
-    return this._http.get("http://chrissrdp2018.sytes.net/weather/api/weather/"+city+'&'+scale)
+    return this._http.get("https://chrissrdp2018.sytes.net/weather/api/weather/"+city+'&'+scale)
      .pipe(map(result => result));
   }
 
